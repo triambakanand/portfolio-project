b7928538b8db5b7394b68a6a6074300fb26a54ef
triambakanand <triambakanand@gmail.com>
2025-02-09 12:43:36 +0530

    start of making

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar: React.FC = () => {
+  return <div style={{ padding: "5rem", color: "#fff" }}>Navbar</div>;
+};
+
+export default Navbar;
--- src/PortFolioHome.tsx
+++ src/PortFolioHome.tsx
@@ -1,7 +1,12 @@
+import Navbar from "./Components/Navbar";
 import "./css/style.css";
 
 const PortFolioHome: React.FC<{}> = ({ ...props }) => {
-  return <></>;
+  return (
+    <div className="master-div">
+      <Navbar />
+    </div>
+  );
 };
 
 export default PortFolioHome;
