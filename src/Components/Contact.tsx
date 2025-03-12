811128f31f64b171d0aa941013f80b3640af8eab
Triambak Anand <triambak.anand@accenture.com>
2025-03-12 13:07:38 +0530

    HashRouter implemented

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,7 +1,21 @@
 import "../css/style.css";
 
 const Contact: React.FC<{}> = ({ ...props }) => {
-  return <>Contact...</>;
+  return (
+    <>
+      <div className="main-div">
+        <span
+          style={{
+            color: "#ae8337",
+            fontSize: "2rem",
+            backgroundColor: "#f7f7f7",
+          }}
+        >
+          Contact - Coming Soon!
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
--- src/Components/Trending.tsx
+++ src/Components/Trending.tsx
@@ -1,7 +1,21 @@
 import "../css/style.css";
 
 const Trending: React.FC<{}> = ({ ...props }) => {
-  return <>Trending...</>;
+  return (
+    <>
+      <div className="main-div">
+        <span
+          style={{
+            color: "#ae8337",
+            fontSize: "2rem",
+            backgroundColor: "#f7f7f7",
+          }}
+        >
+          Trending - Coming Soon!
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default Trending;
