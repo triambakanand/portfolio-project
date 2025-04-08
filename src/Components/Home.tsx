f4b9530de27e835ec0eb54ec0e57203613fc0acd
Triambak Anand <triambak.anand@accenture.com>
2025-04-08 14:58:55 +0530

    Work Experience area completed

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -561,8 +561,7 @@ const Home: React.FC = () => {
             <div className="sec-expe__box"></div>
           </div>
 
-          {/* Sample Code Below*/}
-          <div className="sec-expe__container">
+          <div className="sec-expe__container sec-expe__container--2">
             <div className="sec-expe__grid1">
               <div className="sec-expe__content">
                 <img
@@ -570,13 +569,68 @@ const Home: React.FC = () => {
                   alt="Infy logo"
                   className="sec-expe__companyLogo--2"
                 />
+                <div className="sec-expe__content__cw sec-expe__content__cw--1">
+                  Jan 2023 - Sept 2024
+                </div>
+                <div className="sec-expe__content__details">
+                  <b style={{ color: "#000" }}>Position:</b> Associate
+                  Consultant - Frontend
+                </div>
+                <div className="sec-expe__content__details sec-expe__content__details--1">
+                  <b style={{ color: "#000" }}>Role:</b> Frontend Senior Project
+                  Engineer
+                </div>
+                <div className="sec-expe__content__details">
+                  <b style={{ color: "#000" }}>Project:</b> VERIZON - Optix US
+                </div>
+                <div className="sec-expe__content__details">
+                  <b style={{ color: "#000" }}>Project Description:</b> Verizon
+                  Optix is an internal tool used by verizon agents to place
+                  telecommunication system orders like TV cable, Data cable etc.
+                  There were primarly 3 different applications. We Converted
+                  entire AngularJS based frontend into React and Redux based
+                  frontend by migrating various APIs, UI functionalities and
+                  also adding additional features for optimizing the performance
+                  by using various different methodologies based on react hooks.
+                </div>
+                <div className="sec-expe__content__details sec-expe__content__details--2">
+                  <b style={{ color: "#000" }}>Key Points</b>
+                </div>
+                <div className="sec-expe__content__details sec-expe__content__details--3">
+                  <ul>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Created boiler plate for the entire frontend architecture.
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Managed and integrated end to end code flow for better
+                      code understanding.
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Client interaction over various new features
+                      implementation.
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Provided business solutions to Client for having enhanced
+                      User experience.
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Achieved 90+% coverage in Unit Testing using React Testing
+                      library.
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Collaberated with multiple backend teams, outsourcing
+                      teams, third party application teams to integrate and
+                      build and entire new UI following all code standards.
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="sec-expe__grid2"></div>
+            <div className="sec-expe__grid2 sec-expe__grid2--2"></div>
             <div className="sec-expe__box"></div>
           </div>
 
-          <div className="sec-expe__container">
+          <div className="sec-expe__container sec-expe__container--3">
             <div className="sec-expe__grid1">
               <div className="sec-expe__content">
                 <img
@@ -584,9 +638,75 @@ const Home: React.FC = () => {
                   alt="Persi logo"
                   className="sec-expe__companyLogo--3"
                 />
+                <div className="sec-expe__content__cw sec-expe__content__cw--1">
+                  Nov 2020 - Jan 2023
+                </div>
+                <div className="sec-expe__content__details sec-expe__content__promoted">
+                  Promoted
+                </div>
+                <div className="sec-expe__content__details">
+                  <b style={{ color: "#000" }}>Position:</b> Senior Software
+                  Engineer <span style={{ color: "#28b485" }}>&#8592;</span>{" "}
+                  Software Engineer
+                </div>
+                <div className="sec-expe__content__details"></div>
+                <div className="sec-expe__content__details sec-expe__content__details--1">
+                  <b style={{ color: "#000" }}>Role:</b> Frontend Software
+                  Engineer
+                </div>
+                <div className="sec-expe__content__details">
+                  <b style={{ color: "#000" }}>Project:</b> Alteryx
+                </div>
+                <div className="sec-expe__content__details">
+                  <b style={{ color: "#000" }}>Project Description: </b>Alteryx,
+                  Inc. is software company with offices worldwide. The company's
+                  products are used for data science and analytics. As machine
+                  learning requires a lot of data, we developed connectors of
+                  exact replica of SharePoint, OneDrive, Box, DropBox, Adobe
+                  analytics, GoogleDrive and many more cloud services. Which
+                  fetches user data through APIs and do processing on it. We
+                  developed connectors using REACT-Typescript in the frontend
+                  side and python in the backend.
+                </div>
+                <div className="sec-expe__content__details sec-expe__content__details--2">
+                  <b style={{ color: "#000" }}>Key Points</b>
+                </div>
+                <div className="sec-expe__content__details sec-expe__content__details--3">
+                  <ul>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Translated designs & wireframes into high-quality code
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Provided client business solutions which indeed improved
+                      performance of the applications.
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Developed reusable components fully based on React
+                      functional components
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Communication with client for solving their technical
+                      queries and understanding future feature-enhancement
+                      requests.
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Collaberated with on-shore team for giving insights on how
+                      to implement client requirements
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Achieved 90+% UT coverage for every connector by using
+                      testing libraries Jest, Enzyme, React-testing-library.
+                    </li>
+                    <li className="sec-expe__content__details__bulletPoint">
+                      Supported team members to achieve their tasks before
+                      deadline and helped in providing enhancements in their
+                      code.
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="sec-expe__grid2"></div>
+            <div className="sec-expe__grid2 sec-expe__grid2--3"></div>
             <div className="sec-expe__box"></div>
           </div>
         </section>
